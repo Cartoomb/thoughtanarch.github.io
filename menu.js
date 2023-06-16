4d66a0b908449f5b9fd6671221e432fa746bf0d0
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2023-06-15 17:23:38 -0700

    updates

---
+++ menu.js
@@ -0,0 +1,4 @@
+function menu(){ 
+
+    document.getElementById("header").innerHTML = '<nav class="top-menu"> <section> <img src="./Thoughts.png" style="padding-left: 5%;" width="100px" alt=""> <div class="menu-item"> <a href="./index.html" class="menu-link">Home</a> <a href="./media.html" class="menu-link">Media</a> </div></section></nav> '
+}
